16503617e5e000b9ff42206822c37bbeff7d9f91
Rongdo04 <53899673+Rongdo04@users.noreply.github.com>
2023-03-23 09:26:59 +0700

    Add files via upload

---
+++ WindowFormApp2/Form_Categories.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form_Categories : Form
+    {
+        public Form_Categories()
+        {
+            InitializeComponent();
+        }
+        void GridFill() // FillData
+        {
+            MySqlDataAdapter adapter = new MySqlDataAdapter("select * from mydb.category", Program.connection);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "category");
+            dataGridView1.DataSource = ds.Tables["category"];
+        }
+        private void Form_Categories_Load(object sender, EventArgs e)
+        {
+            
+            Program.connection.Open();
+            GridFill();
+            Program.connection.Close();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) // add
+        {
+            Program.connection.Open();
+            MySqlCommand command = new MySqlCommand("INSERT INTO `mydb`.`category` ( `name`) VALUES (@name);", Program.connection);
+            command.Parameters.Add("@name", MySqlDbType.VarChar).Value = textBox1.Text;
+            try
+            {
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("Query Excuted");
+                }
+                else
+                {
+                    MessageBox.Show("Query not Excuted");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            GridFill();
+
+            Program.connection.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e) //delete
+        {
+            int id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value.ToString());
+            Program.connection.Open();
+            MySqlCommand command = new MySqlCommand("delete_by_id_category", Program.connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("_id", id);
+            command.ExecuteNonQuery();
+            GridFill();
+            Program.connection.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e) // update
+        {
+            Program.connection.Open();
+            int id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value.ToString());
+            MySqlCommand command = new MySqlCommand("update_category", Program.connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("_id", id);
+            command.Parameters.AddWithValue("_name", textBox1.Text);
+            command.ExecuteNonQuery();
+            GridFill();
+            Program.connection.Close();
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.CurrentRow.Index != -1)
+                {
+                    textBox1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e) // cancel
+        { 
+            this.Close();
+        }
+    }
+}
